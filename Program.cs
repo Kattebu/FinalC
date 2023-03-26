498e87e205194a61c207a34995ac5f5a415d7c94
Kattebu <butskoev@tut.by>
2023-03-26 16:07:22 +0300

    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+string[] firstArray = new string[6] {"=)", "cats", "321", "44", "!", "@#$&"};
+string[] secondArray = new string[firstArray.Length];
+
+void GetResultArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i=0; i<firstArray.Length; i++)
+    {
+        if (firstArray[i].Length<=3)
+        {
+            secondArray[count]=firstArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+  for (int i=0; i<array.Length; i++)  
+  {
+    Console.Write($"{array[i]}" + " ");
+  }
+  Console.WriteLine();
+}
+
+GetResultArray(firstArray, secondArray);
+PrintArray(secondArray);
 
